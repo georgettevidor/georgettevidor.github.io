ab6e45a1dc7b214da58e09c3e44b7a4a0ccac41a
Hellon Canella <helloncanella@gmail.com>
2016-03-23 17:57:15 -0300

    ok

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 var svg = {};
 
+var AMARELO = 'amarelo';
+var AZUL = 'azul';
+
+console.log();
+
 function previewFile() {
   var preview = document.querySelector('.foto');
-  console.log(preview);
   var file = document.querySelector('input[type=file]').files[0];
   var reader = new FileReader();
 
   reader.addEventListener("load", function() {
     preview.src = reader.result;
     if (preview.src) {
-      adicionarMascara();
+      colarMascara(AZUL);
     }
   }, false);
 
@@ -18,7 +22,19 @@ function previewFile() {
   }
 }
 
-function adicionarMascara() {
-  var imgWidth = $('img').width();
-  $('.top, .bottom').css('width', imgWidth + 'px');
+function colarMascara(cor) {
+  var caminho = 'imagens/' + cor + '/';
+
+  $('.mascara.top img').attr('src', caminho + 'cima.png');
+  $('.mascara.bottom img').attr('src', caminho + 'baixo.png');
+
+}
+
+
+function download(){
+  html2canvas(document.querySelector('.camarada'), {
+    onrendered: function(canvas) {
+      document.body.appendChild(canvas);
+    }
+  });
 }
